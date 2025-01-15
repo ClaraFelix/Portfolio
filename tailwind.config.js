115ebe89a3b868007b4c3785d6b9070e67e86fa7
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2025-01-15 14:34:21 -0300

    refactor: add colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
-/** @type {import('tailwindcss').Config} */
+const { join } = require("path");
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: [join(__dirname, "src/**/!(*.stories|*.spec).{ts,html}")],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#A78BFA",
+          secondary: "#C4B5FD",
+          neutral: "#DDD6FE",
+          text: "#4C1D95",
+          body: "#F5F3FF",
+          white: "#FFFFF",
+        },
+      },
+    ],
+  },
+  extend: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      mono: ["Roboto", "sans-serif"],
+    },
+    cursor: {
+      grab: "grab",
+      grabbing: "grabbing",
+    },
+  },
 };
