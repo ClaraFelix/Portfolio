f5cece4b63ba3f3f84b473688ff5f4ff3f637537
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2025-05-20 14:01:01 -0300

    update: layout contact

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { InputIconsComponent } from '../../shared/components/input-icons/input-icons.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'portfolio-contact',
   standalone: true,
-  imports: [],
+  imports: [InputIconsComponent, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  userEmail: string = '';
+  userMessage: string = '';
+
+  sendEmail() {
+    const email = 'clara.patriciafelix@hotmail.com';
+    const subject = encodeURIComponent('Contato via Portfólio');
+    const body = encodeURIComponent(
+      `De: ${this.userEmail}\n\n${this.userMessage}`
+    );
+    const mailtoUrl = `mailto:${email}?subject=${subject}&body=${body}`;
+
+    window.location.href = mailtoUrl;
+  }
+}
