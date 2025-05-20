4344d5e2f95fc32b22dafbbdd5318308ea9e5b0f
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2025-05-20 13:57:50 -0300

    feat: add training component

---
+++ src/app/pages/training/training.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-training',
+  standalone: true,
+  imports: [],
+  templateUrl: './training.component.html',
+  styleUrl: './training.component.scss',
+})
+export class TrainingComponent {}
