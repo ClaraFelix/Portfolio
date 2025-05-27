1e42b10eb456e9ab0216eb7109d077068c0827ad
Clara Felix <80846078+ClaraFelix@users.noreply.github.com>
2025-05-27 14:24:40 -0300

    feat: add ControlValueAccessor on input component

--- src/app/shared/components/input-icons/input-icons.component.ts
+++ src/app/shared/components/input-icons/input-icons.component.ts
@@ -1,17 +1,51 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 export type HTMLInputTypeAttribute = 'text' | 'email' | 'number' | 'tel';
 
 @Component({
   selector: 'portfolio-input-icons',
   standalone: true,
-  imports: [],
   templateUrl: './input-icons.component.html',
   styleUrls: ['./input-icons.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputIconsComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputIconsComponent {
+export class InputIconsComponent implements ControlValueAccessor {
   @Input() placeholder = '';
-  @Input() value = '';
   @Input() type: HTMLInputTypeAttribute = 'text';
   @Input() disabled = false;
+
+  value: string = '';
+
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  handleInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
 }
